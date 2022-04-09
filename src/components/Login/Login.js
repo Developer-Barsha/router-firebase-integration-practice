0b10c46e06f18b26200ed04bee5da28dfb7229e6
Developer-Barsha <lamiatabassumbarsha@gmail.com>
2022-04-09 12:43:07 +0600

    react problem solved

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -5,11 +5,14 @@ const Login = () => {
     return (
         <div>
             <h2>PLease Login</h2>
+            <div style={{margin:'10px 0'}}>
+                <button>Google Sign In</button>
+            </div>
             <form>
                 <input type="email" placeholder='Your Email' />
-                <br/>
-                <input type="password" name="" id="" placeholder='Your Password'/>
-                <br/>
+                <br />
+                <input type="password" name="" id="" placeholder='Your Password' />
+                <br />
                 <input type="submit" value="Login" />
             </form>
         </div>
